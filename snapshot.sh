5f9697151025e7da6b348518813c4af1c0ea9a6a
lpuskas <laszlo.puskas@sequenceiq.com>
2014-12-04 16:29:59 +0100

    CLOUD-331 removed liquibase from the project

--- snapshot.sh
+++ snapshot.sh
@@ -15,22 +15,6 @@ elif [ "$2" = "get" ]; then
   tar -xf "/tmp/${FILE_NAME}" -C /
 
   echo "Done.";
-
-elif [ "$2" = "migrate" ]; then
-
-  echo "Getting liquibase changelogs ..."
-  aws s3 cp "s3://liquibase-changelogs/cloudbreak-changelogs.tar" /tmp
-
-  echo "Unpacking changelogs..."
-  tar -xf "/tmp/cloudbreak-changelogs.tar" -C /
-
-  source /setup_liquibase.sh;
-
-  echo "Updating database..."
-  liquibase --changeLogFile="/cloudbreak-changelogs/cb_changelog.xml" update
-
-  echo "Done."
-
 else
   echo "Invalid or no argument provided";
 fi
